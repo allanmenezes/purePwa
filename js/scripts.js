76ac35a92e43347dcf10e396611d0b753fd2ef26
Allan Menezes Proton <allan.fm@outlook.com>
2019-05-23 18:30:19 -0300

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -100,3 +100,15 @@ if ('serviceWorker' in navigator) {
         console.log('Erro', err);
     });
 }
+
+// Detecta se o dispositivo está no iOS
+const isIos = () => {
+  const userAgent = window.navigator.userAgent.toLowerCase();
+  return /iphone|ipad|ipod/.test( userAgent );
+}
+// Detects if device is in standalone mode
+const isInStandaloneMode = () => ('standalone' in window.navigator) && (window.navigator.standalone);
+// Verifica se deve exibir notificação popup de instalação:
+if (isIos() && !isInStandaloneMode()) {
+  this.setState({ showInstallMessage: true });
+}
